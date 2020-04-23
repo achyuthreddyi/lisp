8f61ec83a21dba6479dc5e65eb1ac0e8c6f19512
achyuth <achyuth@localhost.localdomain>
2020-04-23 06:17:10 +0530

    refactored the complete code and implemented the complete structure

--- lisp.js
+++ lisp.js
@@ -1,81 +1,32 @@
 let globalenv = {
-  '+' : (args) => args.reduce((a,b) => a+b),
+  '+': (args) => args.reduce((a,b) => a+b),
   '-' : (args) => args.reduce((a,b) => a-b),
-  '*' :(args) => args.reduce((a,b) => a*b),
-  '/' :(args) => args.reduce((a,b) => a/b),
-  'pi'  : Math.PI
+  '*' :  (args) => args.reduce((a,b) => a*b),
+  '/': (args) => args.reduce((a,b) => a/b),
+  'pi': Math.PI
 }
-// console.log('typeof',typeof(globalenv['pi']))
-
-function skipSpace(string) {  
-  let first = string.search(/\S/);
-  if (first == -1) return "";
-  return string.slice(first);
-} // end of skip fucnction 
-
-function numberParser (inp) {
-  let result = inp.match(/^-?(0|[\d1-9]\d*)(\.\d+)?(?:[Ee][+-]?\d+)?/)
-  if (result === null) return null
-  return [Number(result[0]) , skipSpace(inp.slice(result[0].length))]
-}// end of numberparser
-
-
-function operation(inp,env=globalenv){
-  let result
-  let args= []
-
-  if(inp.startsWith('(')){
-
-    inp =skipSpace (inp.slice(1))
-    let op = inp.slice(0,inp.indexOf(' '))
-    // console.log(op);
-    if(!globalenv[op]) return 'nosuch operatio allowed'
-    // if(globalenv[op] == Math.PI) return globalenv[op]
-    // console.log(typeof(globalenv[op]));
-    inp = skipSpace(inp.slice(op.length))
-
-
-    while (inp[0] != ')'){
-      console.log('input',inp[0]);
-      if (inp.startsWith('pi')){
-        return (result = [Number(3.1415) , inp  = inp.slice(2)])
-      }
-      
-      else result = identify(inp, env)
-      console.log('result:',result);
-      
-      if (!result) return 'no result '
-      console.log('result[0',result[0]);
-      
-      args.push(result[0])
-      console.log('args',args);
-      
-      inp  = skipSpace(result[1])
-    }// end of while loop
-    
-    
-    return [globalenv[op](args),inp.slice(1)]
-
-  } // end of if loop 
-  else return null 
+function specialform_parser(input){
+  // comprises of define lambda quote begin if 
+  return null
 
 }
-
-
-
-function identify(inp,env = globalenv){
-  //  can be an expression or the special forms
-inp = skipSpace(inp)
-let result = operation(inp, globalenv) || numberParser(inp) || piparser(inp)
-return result
-   
+function expression_parser(input){
+  // comprises of operations
+  return null
 
 }
-// main function for the interpreter
+function numberparser(data){
+  let result = /[-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/.exec(data);
+  if ( result != null && result.index === 0) return ([Number(result[0]), data.trim().slice(result[0].length)]);
+  return null;
+
+}
 function evaluate(input){
-  let output = identify(input,globalenv)
-  return (output =='' ? 'invalid':output)
+  // comprises of specialform parser, expressionprser, numberparser
+
+  let result
+  result = specialform_parser(input) || expression_parser(input) || numberparser(input)
+  return result
 }
-
-console.log(evaluate('(+ 1 pi)'));
-
+console.log(evaluate('45'));
+// (evaluate('45'))
